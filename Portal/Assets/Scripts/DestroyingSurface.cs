c4beb46a685a506c81128a95e1491395347ae629
Isurpri <hamsisaac@gmail.com>
2025-11-13 09:43:31 +0100

    SolutionRefractionCube/AllLevelDone

--- Portal/Assets/Scripts/DestroyingSurface.cs
+++ Portal/Assets/Scripts/DestroyingSurface.cs
@@ -1,11 +1,20 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DestroyingSurface : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
-    {        
-        if(other.CompareTag("Cube"))
+    {
+        if (other.CompareTag("Cube"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        else if (other.CompareTag("RefractionCube"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        else if(other.CompareTag("Turret"))
         {
             other.gameObject.SetActive(false);
         }
